13571a5828db811b89f7ae2a2dfed2bb21df6f73
lucas iscovici <lucas.iscovici@moonhealth.ai>
2021-02-10 03:00:57 +0100

    refactor login

--- template/src/Containers/Auth/Login.js
+++ template/src/Containers/Auth/Login.js
@@ -1,11 +1,60 @@
 import React from 'react'
-import { Box, Text } from '@/Theme'
+import { Box, Text, Button, TextInput } from '@/Theme'
+import { useAuth } from '@/Services/Auth'
+
+import SafeAreaView2 from '@/Components/SafeAreaView2'
 
 const Login = (props) => {
+  const { loginWithUsernamePassword } = useAuth()
+  const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const tiRef = React.useRef(null)
+
+  React.useEffect(() => {
+    tiRef.current?.focus()
+  }, [])
+
+  const onClick = () => {
+    if (username.length > 0 && password.length > 0) {
+      try {
+        loginWithUsernamePassword(username, password)
+      } catch (e) {
+        console.log(e)
+        setError(e)
+      }
+    }
+  }
   return (
-    <Box>
-      <Text>Login</Text>
-    </Box>
+    <SafeAreaView2>
+      <Box flex={1} variant="colCenter" px="s">
+        <Text variant="title" mb="auto" flex={0}>
+          Login
+        </Text>
+        <Box w="max" px={6} flex={1} variant="colCenter">
+          <TextInput
+            variant="login"
+            placeholder="Username"
+            autofocus={true}
+            ref={tiRef}
+            onChangeText={(text) => setUsername(text)}
+          />
+
+          <TextInput
+            variant="login"
+            placeholder="Password"
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Button
+            variant="primary"
+            title="Connexion"
+            onPress={onClick}
+            mt="s"
+          />
+        </Box>
+        {error && <Text color="red">{error}</Text>}
+      </Box>
+    </SafeAreaView2>
   )
 }
 
