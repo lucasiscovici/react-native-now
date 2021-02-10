22d542c6806c48780993df0810a95b4da6255b6b
lucas iscovici <lucas.iscovici@moonhealth.ai>
2021-02-10 03:02:07 +0100

    donct getUser in lauch

--- template/src/State/Startup/actions.js
+++ template/src/State/Startup/actions.js
@@ -1,12 +1,12 @@
 // export const ACTION = async ({}) => {};
 import { actions as themeActions } from '@/State/Theme'
-import { actions as userActions } from '@/State/User'
+// import { actions as userActions } from '@/State/User'
 
 export const launch = async () => {
   // Timeout to fake waiting some process
   // Remove it, or keep it if you want display a beautiful splash screen ;)
   await new Promise((resolve) => setTimeout(resolve, 1000))
   // Here we load the user 1 for example, but you can for example load the connected user
-  await userActions.getUser({ userId: 1 })
+  // await userActions.getUser({ userId: 1 })
   await themeActions.setDefaultTheme({ theme: 'default', darkMode: null })
 }
