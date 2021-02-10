801680aae421be8d2305cb964eb630cd4a77269a
lucas iscovici <lucas.iscovici@moonhealth.ai>
2021-02-10 03:06:54 +0100

    add auth index

---
+++ template/src/Services/Auth/index.js
@@ -0,0 +1,19 @@
+import { Config } from '@/Config'
+import { configure, useAuth } from './auth-service'
+
+const callbackAfterLogout = () => {
+  const { store, clearState } = require('@/State/store')
+  store.dispatch(clearState)
+}
+
+const { config } = configure({
+  Authorization: 'Bearer',
+  callbackAfterLogout: callbackAfterLogout,
+  clientId: Config.clientId,
+  clientSecret: Config.clientSecret,
+  baseUrlAuth: Config.AUTH_URL,
+  textAlertBeforeExpiredLogout: 'Session Expiré, Veuillez vous reconnecter.',
+  textAlertBeforeLogout: 'Vous êtes bien déconnecté',
+})
+
+export { config, useAuth }
