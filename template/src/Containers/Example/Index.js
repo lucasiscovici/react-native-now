2be78387322d755bbc973cb689e302b46cf41de6
lucas iscovici <lucas.iscovici@moonhealth.ai>
2021-02-10 03:01:11 +0100

    refactor exemple

--- template/src/Containers/Example/Index.js
+++ template/src/Containers/Example/Index.js
@@ -9,15 +9,21 @@ import {
   selectors as userSelectors,
 } from '@/State/User'
 
-import { Box, Text, TextInput, Button } from '@/Theme'
+import { Box, Text, TextInput, Button, useTheme } from '@/Theme'
 
+import { useAuth } from '@/Services/Auth'
+
+import SafeAreaView2 from '@/Components/SafeAreaView2'
 const IndexExampleContainer = () => {
   const { t } = useTranslation()
 
   const user = userSelectors.get()
-  const fetchOneUserLoading = userSelectors.isPending()
-
+  const isLoading = userSelectors.isPending()
   const error = userSelectors.getError()
+
+  const { logout } = useAuth()
+
+  const theme = useTheme()
 
   const [userId, setUserId] = useState('1')
 
@@ -28,71 +34,78 @@ const IndexExampleContainer = () => {
     }
   }
 
-  const changeTheme = async ({ theme, darkMode }) =>
+  const changeTheme = async ({ darkMode }) =>
     ThemeActions.changeTheme({ theme, darkMode })
 
   return (
-    <Box flex={1} variant="colCenter" px="s">
-      <Box variant="colCenter" px="s">
-        <Brand />
-        {fetchOneUserLoading && <ActivityIndicator />}
-        {error ? (
-          <Text fSize="m">{error}</Text>
-        ) : (
-          <Text fSize="m" textAlign="center">
-            {t('example.helloUser', { name: `\n${user?.name ?? ''}` })}
-          </Text>
-        )}
+    <SafeAreaView2 bgCol={theme.Colors.purpleDark}>
+      <Box flex={1} variant="colCenter" backgroundColor="purpleDark">
+        <Box flex={1} variant="colCenter" px="s" backgroundColor="purpleDark">
+          <Box variant="colCenter" px="s">
+            <Brand />
+            {isLoading && <ActivityIndicator />}
+            {error ? (
+              <Text fSize="m">{error}</Text>
+            ) : (
+              <Text fSize="m" textAlign="center">
+                {t('example.helloUser', { name: `\n${user?.name ?? ''}` })}
+              </Text>
+            )}
+          </Box>
+          <Box
+            flexDirection="column"
+            alignItems="center"
+            px="s"
+            py="s"
+            my="l"
+            backgroundColor="primary"
+            borderRadius={5}
+          >
+            <Text textAlign="center">{t('example.labels.userId')}</Text>
+            <TextInput
+              variant="home"
+              onChangeText={(text) => fetch(text)}
+              editable={!isLoading}
+              keyboardType={'number-pad'}
+              maxLength={2}
+              value={userId}
+              selectTextOnFocus
+              px="s"
+            />
+          </Box>
+          <Text fSize="m">DarkMode :</Text>
+          <Button
+            onPress={() => changeTheme({ darkMode: null })}
+            title="Auto"
+            borderColor={'black'}
+            borderWidth={1}
+            alignItems="center"
+            w="xxl"
+          />
+          <Button
+            onPress={() => changeTheme({ darkMode: true })}
+            title="Dark"
+            borderColor={'white'}
+            borderWidth={1}
+            alignItems="center"
+            w="xxl"
+            backgroundColor="black"
+            textProps={{ color: 'primary' }}
+          />
+          <Button
+            onPress={() => changeTheme({ darkMode: false })}
+            title="Light"
+            borderColor={'black'}
+            borderWidth={1}
+            alignItems="center"
+            w="xxl"
+          />
+        </Box>
+        <Box flex={0} backgroundColor="red" justifyContent="flex-end">
+          <Button variant="primary" title="logout" onPress={() => logout()} />
+        </Box>
       </Box>
-      <Box
-        flexDirection="column"
-        alignItems="center"
-        px="s"
-        py="s"
-        my="l"
-        backgroundColor="primary"
-        borderRadius={5}
-      >
-        <Text textAlign="center">{t('example.labels.userId')}</Text>
-        <TextInput
-          variant="home"
-          onChangeText={(text) => fetch(text)}
-          editable={!fetchOneUserLoading}
-          keyboardType={'number-pad'}
-          maxLength={2}
-          value={userId}
-          selectTextOnFocus
-          px="s"
-        />
-      </Box>
-      <Text fSize="m">DarkMode :</Text>
-      <Button
-        onPress={() => changeTheme({ darkMode: null })}
-        title="Auto"
-        borderColor={'black'}
-        borderWidth={1}
-        alignItems="center"
-        w="xxl"
-      />
-      <Button
-        onPress={() => changeTheme({ darkMode: true })}
-        title="Dark"
-        borderColor={'white'}
-        borderWidth={1}
-        alignItems="center"
-        w="xxl"
-        backgroundColor="black"
-        textProps={{ color: 'primary' }}
-      />
-      <Button
-        onPress={() => changeTheme({ darkMode: false })}
-        title="Light"
-        borderColor={'black'}
-        borderWidth={1}
-        alignItems="center"
-        w="xxl"
-      />
-    </Box>
+    </SafeAreaView2>
   )
 }
 
