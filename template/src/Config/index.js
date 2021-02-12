f4fb7293dc4d47aad0345c1a3678c072fd8d8dce
lucas iscovici <lucas.iscovici@moonhealth.ai>
2021-02-13 00:28:45 +0100

    set default index

--- template/src/Config/index.js
+++ template/src/Config/index.js
@@ -1,3 +1,11 @@
 export const Config = {
   API_URL: 'https://jsonplaceholder.typicode.com/',
+  // auth
+  auth: true,
+  AUTH_URL: 'https://jsonplaceholder.typicode.com/',
+  clientId: '',
+  clientSecret: '',
+  // Social - AUTH
+  facebook: {},
+  google: {},
 }
